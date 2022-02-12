79cfd32d50837af5968c37465e4999b1f9d3dd12
Redwars22 <andrewpereira.megabyte@gmail.com>
2022-02-12 12:04:11 -0300

    Jogo da Memória em C com alocação dinâmica de matriz - Lógica (Modo Easy)

--- C/jogo_da_memoria_com_alocacao_de_matriz.c
+++ C/jogo_da_memoria_com_alocacao_de_matriz.c
@@ -2,12 +2,23 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-int choice, **M = NULL, n = 0, m = 0;
+//Constantes
+#define EASY_MODE 4
+#define HARD_MODE 8
+
+//Variáveis
+int choice, **M = NULL, n = 0, m = 0, score = 0, argument = 0, upper, lower, index_i, index_j, value;
+char gameMode;
 
 //n = linhas, m = colunas
 
 void game(int, int);
+void easyMode(int, int);
+void hardMode(int, int);
+void setGameMode();
+void getScore(int, int);
 
 int main()
 {
@@ -21,20 +32,27 @@ int main()
     
     switch(choice){
         case 1:
-            n = 4;
-            m = 4;
+            n = EASY_MODE;
+            m = EASY_MODE;
+            gameMode = 'E';
             game(n, m);
+            break;
         case 2:
+            n = HARD_MODE;
+            m = HARD_MODE;
+            gameMode = 'H';
+            game(n, m);
             break;
         default:
             break;
     }
-    
-    
+
     return 0;
 }
 
 void game(int rows, int columns){
+    system("clear");
+
     M = (int**) malloc(rows * sizeof(int*));
     
     for(int i = 0; i < rows; i++){
@@ -51,10 +69,74 @@ void game(int rows, int columns){
             }
         }
     }
-    
-    /*for(int l = 0; l < rows; l++){
+
+    printf("Esta é a matriz. Memorize-a e pressione qualquer tecla para continuar!\n\n");
+    setGameMode();
+}
+
+void setGameMode(){
+    if(gameMode == 'E'){
+        easyMode(EASY_MODE, EASY_MODE);
+    } else {
+        hardMode(HARD_MODE, HARD_MODE);
+    }
+}
+
+void easyMode(int rows, int columns){
+    //Printa a matriz
+    for(int l = 0; l < rows; l++){
+        for(int p = 0; p < columns; p++){
+            printf("%d", M[l][p]);
+            if(p == (columns - 1))
+                printf("\n");
+        }
+    }
+
+    //Espera o usuário memorizar a matriz e decidir continuar
+    do {
+        printf("\nDigite 1 para continuar... \n->");
+        scanf("%d", &argument);
+    } while(argument != 1);
+
+    if(argument == 1)
+        system("clear");
+
+    //Seta o mínimo e o máximo do índice da matriz
+    upper = (EASY_MODE - 1);
+    lower = 0;
+
+    //A parte do jogo em si
+    for(int arg = 0; arg < 16; arg++){
+        index_i = (rand() % (upper - lower + 1)) + lower;
+        index_j = (rand() % (upper - lower + 1)) + lower;
+
+        printf("\nValor de M[%d][%d]: ", index_i, index_j);
+        scanf("%d", &value);
+        if(value == M[index_i][index_j]){
+            printf("CORRETO!!! +5 pontos");
+            score += 5;
+        } else {
+            printf("ERRADO!!!");
+        }
+    }
+
+    //Exibe a pontuação total e espera o usuário digitar algo para retornar ao menu principal
+    system("clear");
+    printf("\nPONTUAÇÃO TOTAL: %d", score);
+    do {
+        printf("\nDigite 1 para continuar... \n->");
+        scanf("%d", &argument);
+    } while(argument != 1);
+
+    if(argument == 1)
+        system("clear");
+        main();
+}
+
+void hardMode(int rows, int columns){
+    for(int l = 0; l < rows; l++){
         for(int p = 0; p < columns; p++){
             printf("%d", M[l][p]);
         }
-    }*/
+    }
 }
