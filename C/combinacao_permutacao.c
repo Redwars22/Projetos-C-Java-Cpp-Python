fb4fcd8e26939d09dfa7d58958ddb695ac4b7fc4
Redwars22 <andrewpereira.megabyte@gmail.com>
2022-02-14 23:18:17 -0300

    Cálculo de fatorial

--- C/combinacao_permutacao.c
+++ C/combinacao_permutacao.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int option;
+int option, value;
 
 void mainMenu();
 void combinacao();
@@ -13,5 +13,17 @@ int main(int argc, char const *argv[])
     printf("Escolha uma opção: ");
     printf("\n1-COMBINAÇÃO SIMPLES\n2-COMBINAÇÃO COMPOSTA\n3-PERMUTAÇÃO");
     scanf("%d", &option);
+    permutacao();
     return 0;
+}
+
+void permutacao(){
+    system("clear");
+    printf("PERMUTAÇÃO: Insira a quantidade de elementos:");
+    scanf("%d", &value);
+    int result = 1;
+    for(int i = value; i > 0; i--){
+        result *= i;
+    }
+    printf("%d", result);
 }
