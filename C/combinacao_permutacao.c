9684d26723c5ef7784bdb8894a13808bddf9daa4
Redwars22 <andrewpereira.megabyte@gmail.com>
2022-02-14 22:58:13 -0300

    Calculadora - Combinação e Permutação: Código Base

--- C/combinacao_permutacao.c
+++ C/combinacao_permutacao.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int option;
+
 void mainMenu();
 void combinacao();
 void permutacao();
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    printf("CALCULADORA - COMBINAÇÃO/PERMUTAÇÃO");
+    printf("Escolha uma opção: ");
+    printf("\n1-COMBINAÇÃO SIMPLES\n2-COMBINAÇÃO COMPOSTA\n3-PERMUTAÇÃO");
+    scanf("%d", &option);
     return 0;
 }
