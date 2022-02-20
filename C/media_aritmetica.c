f21c83e901da731aef916b50db0f686c46eef4ef
Redwars22 <andrewpereira.megabyte@gmail.com>
2022-02-20 00:20:50 -0300

    Lógica da Calculadora de Média em C

--- C/media_aritmetica.c
+++ C/media_aritmetica.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+int total, sum, aux, result;
 
 int main()
 {
-    /* code */
+    printf("#####[ CALCULADORA DE MÉDIA EM C ]#####");
+    while (true)
+    {
+        printf("\nQuantidade de elementos: ");
+        scanf("%d", &total);
+
+        if (total == 0)
+            break;
+
+        for (int i = 0; i < total; i++)
+        {
+            printf("x%d:", i + 1);
+            scanf("%d", &aux);
+            sum += aux;
+        }
+        
+        result = (sum / total);
+        printf("Média: %d\n", result);
+    }
     return 0;
 }
