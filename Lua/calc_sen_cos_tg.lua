042cae384f712d68b09de40be1cb10d38b9151eb
Redwars22 <andrewpereira.megabyte@gmail.com>
2022-02-20 19:09:57 -0300

    Calculadora Seno, Cosseno, Tangente - Biblioteca Math

--- Lua/calc_sen_cos_tg.lua
+++ Lua/calc_sen_cos_tg.lua
@@ -1,3 +1,20 @@
-print("Digite o valor em graus (0 a 360):")
-value = io.read()
-print(value)
+function convertToRadians(value)
+    return math.rad(value)
+end
+
+function GetSinCosTan(radians)
+    print("SENO: " .. math.sin(radians))
+    print("COSSENO: " .. math.cos(radians))
+    print("TANGENTE: " .. math.tan(radians))
+    print("-------------------------------")
+end
+
+print("######[ CALCULADORA SENO, COSSENO, TANGENTE ]######")
+while true do
+    print("Digite o valor em graus (0 a 360):")
+    value = io.read()
+    radians = convertToRadians(value)
+    GetSinCosTan(radians)
+end
+
+--lua calc_sen_cos_tg.lua
