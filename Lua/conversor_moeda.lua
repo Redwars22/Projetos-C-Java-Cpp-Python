76b2abe393f56f50a7e8f75522f468e18fc215b9
Redwars22 <andrewpereira.megabyte@gmail.com>
2022-02-25 12:47:14 -0300

    Conversor de Moeda em Lua

---
+++ Lua/conversor_moeda.lua
@@ -0,0 +1,37 @@
+currency = 1.53;
+
+function GetKey()
+    print("\nDigite qualquer valor e pressione ENTER para continuar...")
+    x = io.read()
+end
+
+function DollarToRoblux()
+    print("CONVERTER DE DÓLAR PARA ROBLUX\n$ ")
+    dollarValue = io.read();
+    result = dollarValue / currency;
+    print("VALOR EM ROBLUX: RBX " .. result)
+end
+
+function RobluxToDollar()
+    print("CONVERTER DE ROBLUX PARA DÓLAR\nRBX ")
+    robluxValue = io.read();
+    result = robluxValue * currency;
+    print("VALOR EM DÓLAR: $ " .. result)
+end
+
+while true do
+    os.execute('clear')
+    print("CONVERSOR DE MOEDAS\n")
+    print("1-RBX para USD\n2-USD para RBX\n3-SAIR\n->")
+    option = io.read()
+
+    if option == '1' then
+        RobluxToDollar()
+        GetKey()
+    elseif option == '2' then
+        DollarToRoblux()
+        GetKey()
+    elseif option == '3' then
+        break
+    end
+end
