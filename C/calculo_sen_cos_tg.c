f1bcb1eb9bf1c8d9fb730cb85e78ca87662c17ad
Redwars22 <andrewpereira.megabyte@gmail.com>
2022-02-25 10:49:23 -0300

    Projetos C + Projetos C++ e Lua (Rascunho)

--- C/calculo_sen_cos_tg.c
+++ C/calculo_sen_cos_tg.c
@@ -1,1 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
 
+#define PI  3.14159265
+
+double value, inRadians, sine, cosin, tang;
+
+int main()
+{
+    printf("#####[ TRIGONOMETRIA EM C ]#####\n");
+    while(true){
+        printf("INSIRA UM VALOR EM GRAUS: ");
+        scanf("%lf", &value);
+        inRadians = value * (PI/180);
+        printf("\nSENO: %lf\n", sin(inRadians));
+        printf("COSSENO: %lf\n", cos(inRadians));
+        printf("TANGENTE: %lf\n\n", tan(inRadians));
+        for(int i = 0; i < 12; i++) printf("#-#");
+        printf("\n");
+    }
+    return 0;
+}
